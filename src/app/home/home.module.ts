d09b0860cb77b5e7a8f3f088628e874c6f015c45
molibdy <molipotts@gmail.com>
2021-05-04 12:35:44 +0200

    navBar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,
+  NavBarComponent]
 })
 export class HomePageModule {}

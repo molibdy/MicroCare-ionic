0b0baad39ef0394fcbc0d5b88b8c9ebfe6d2c28c
Fran Martinez <fran.martinez.torres@gmail.com>
2021-05-05 15:55:02 +0200

    l

--- src/app/pages/preferencias/preferencias.page.ts
+++ src/app/pages/preferencias/preferencias.page.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
+// import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {ElementRef, ViewChild} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
-import {MatChipInputEvent} from '@angular/material/chips';
+// import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+// import {MatChipInputEvent} from '@angular/material/chips';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 
